7a3dc01f31c44af144f343d8c82b9f9f4e53bbf4
Jakob <jakobmsgrant@gmail.com>
2020-09-04 08:56:24 -0700

    refactored passing code

--- src/components/__tests__/IcosahedralList.spec.js
+++ src/components/__tests__/IcosahedralList.spec.js
@@ -3,30 +3,30 @@ import { render } from '@testing-library/react';
 import { IcosahedralList } from '../IcosahedralList';
 
 describe('IcosahedralList', () => {
-  it('loads icosahedral names on first render', () => {
-    const loadIcosahedrons = jest.fn().mockName('loadIcosohedrons');
-    const icosahedrons = [];
+  const icosahedrons = [
+    { id: 1, name: 'CREATIVE GENESIS' },
+    { id: 2, name: 'PRIMAL MATRIX' },
+  ];
+  let loadIcosahedrons;
+  let context;
 
-    render(
+  beforeEach(() => {
+    loadIcosahedrons = jest.fn().mockName('loadIcosahedrons');
+
+    context = render(
       <IcosahedralList
         loadIcosahedrons={loadIcosahedrons}
         icosahedrons={icosahedrons}
       />,
     );
+  });
 
+  it('loads icosahedral names on first render', () => {
     expect(loadIcosahedrons).toHaveBeenCalled();
   });
 
   it('displays the icosahedral names', () => {
-    const noop = () => {};
-    const icosahedrons = [
-      { id: 1, name: 'CREATIVE GENESIS' },
-      { id: 2, name: 'PRIMAL MATRIX' },
-    ];
-
-    const { queryByText } = render(
-      <IcosahedralList loadIcosahedrons={noop} icosahedrons={icosahedrons} />,
-    );
+    const { queryByText } = context;
 
     expect(queryByText('CREATIVE GENESIS')).not.toBeNull();
     expect(queryByText('PRIMAL MATRIX')).not.toBeNull();
