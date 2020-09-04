03bbc914f475ab9b73c1ab486be4847d1adb8b09
Jakob <jakobmsgrant@gmail.com>
2020-09-04 15:26:53 -0700

    unit test the store

--- src/store/icosahedrons/actions.js
+++ src/store/icosahedrons/actions.js
@@ -1,1 +1,12 @@
-export const loadIcosahedrons = () => () => {};
+export const STORE_ICOSAHEDRONS = 'STORE_ICOSAHEDRONS';
+
+export const loadIcosahedrons = () => (dispatch, getState, api) =>{
+  api.loadIcosahedrons().then(records => {
+    dispatch(storeIcosahedrons(records));
+  });
+};
+
+const storeIcosahedrons = records => ({
+  type: STORE_ICOSAHEDRONS,
+  records,
+});
--- src/store/icosahedrons/reducers.js
+++ src/store/icosahedrons/reducers.js
@@ -1,6 +1,14 @@
 import {combineReducers} from 'redux';
+import {STORE_ICOSAHEDRONS} from './actions';
 
-const records = () => [];
+const records = (state = [], action) => {
+  switch (action.type) {
+    case STORE_ICOSAHEDRONS:
+      return action.records;
+    default:
+      return state;  
+  }
+};
 
 export default combineReducers({
   records,
