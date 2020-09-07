91a4ef09af4eaff02a9a0e118ea2c3a065f4f9ce
Jakob <jakobmsgrant@gmail.com>
2020-09-07 09:10:12 -0700

    changed test.yml to use updated cypress

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/icoHexSequences', (req, res) => {
   res.status(200).json([
