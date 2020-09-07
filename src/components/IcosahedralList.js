3a3ed7d23209b8582709f4320300efe6d563e385
Jakob <jakobmsgrant@gmail.com>
2020-09-07 11:57:45 -0700

    added some material-ui styling

--- src/components/IcosahedralList.js
+++ src/components/IcosahedralList.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
 import {connect} from 'react-redux';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import {loadIcosahedrons} from '../store/icosahedrons/actions';
 
 export const IcosahedralList = ({ loadIcosahedrons, icosahedrons }) => {
@@ -8,13 +11,17 @@ export const IcosahedralList = ({ loadIcosahedrons, icosahedrons }) => {
   }, [loadIcosahedrons]);
 
   return (
-    <ul>
+    <List>
       {icosahedrons.map(icosahedron => (
-        <li key={icosahedron.id}>{icosahedron.name}</li>
-      ))}
-    </ul>
-  );
-};
+        <ListItem key={icosahedron.id}>
+          <ListItemText>
+            {icosahedron.name}
+          </ListItemText>
+        </ListItem>
+        ))}
+    </List>
+    );
+  };
 
 const mapStateToProps = state => ({
   icosahedrons : state.icosahedrons.records,
