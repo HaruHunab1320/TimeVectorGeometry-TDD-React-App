62be82b6debe985d47c008a2253d7239ae5f565a
Jakob <jakobmsgrant@gmail.com>
2020-09-07 21:57:29 -0700

    test to check if loading is true

--- src/store/__test__/icosahedrons.spec.js
+++ src/store/__test__/icosahedrons.spec.js
@@ -4,30 +4,52 @@ import icosahedronsReducer from '../icosahedrons/reducers';
 import { loadIcosahedrons } from '../icosahedrons/actions';
 
 describe('icosahedrons', () => {
-  describe('loadIcosahedrons action', ()=>{
-    it('stores the icosahedrons', async () => {
-      const records = [
-        {id: 1, name: 'CREATIVE GENESIS'},
-        {id: 2, name: 'PRIMAL MATRIX'},
-      ];
+  describe('loadIcosahedrons action', () => {
+    describe('when loading succeeds', () => {
+      it('stores the icosahedrons', async () => {
+        const records = [
+          {id: 1, name: 'CREATIVE GENESIS'},
+          {id: 2, name: 'PRIMAL MATRIX'},
+        ];
 
-      const api = {
-        loadIcosahedrons: () => Promise.resolve(records),
-      };
+        const api = {
+          loadIcosahedrons: () => Promise.resolve(records),
+        };
 
-      const initialState = {
-        records: [],
-      };
+        const initialState = {
+          records: [],
+        };
 
-      const store = createStore(
-        icosahedronsReducer,
-        initialState,
-        applyMiddleware(thunk.withExtraArgument(api)),
-      );
+        const store = createStore(
+          icosahedronsReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        );
 
-      await store.dispatch(loadIcosahedrons());
+        await store.dispatch(loadIcosahedrons());
 
-      expect(store.getState().records).toEqual(records);
+        expect(store.getState().records).toEqual(records);
+      });
+
+      describe('while loading', () => {
+        it('sets a loading flag', () => { 
+          const api = {
+            loadIcosahedrons: () => new Promise(()=> {}),
+          };
+
+          const initialState = {};
+
+          const store = createStore(
+            icosahedronsReducer,
+            initialState,
+            applyMiddleware(thunk.withExtraArgument(api)),
+          );
+
+          store.dispatch(loadIcosahedrons());
+
+          expect(store.getState().loading).toEqual(true)
+        });
+      })
     });
   });
 });
