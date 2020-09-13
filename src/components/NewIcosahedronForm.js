1d453ae60c429b4320bfb6fa20551af8326374ed
Jakob <jakobmsgrant@gmail.com>
2020-09-13 12:34:30 -0700

    seconds feature complete, Can now add item to the list

--- src/components/NewIcosahedronForm.js
+++ src/components/NewIcosahedronForm.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Alert from '@material-ui/lab/Alert';
 import { createIcosahedron } from '../store/icosahedrons/actions';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 export const NewIcosahedronForm = ({ createIcosahedron }) => {
+  const classes = useStyles();
   const [name, setName] = useState('');
   const [validationError, setValidationError] = useState(false);
   const [serverError, setServerError] = useState(false);
@@ -36,22 +47,24 @@ export const NewIcosahedronForm = ({ createIcosahedron }) => {
         </Alert>
       )}
       {validationError && <Alert severity="error">Name is required</Alert>}
-      <TextField
-        value={name}
-        onChange={e => setName(e.target.value)}
-        placeholder="Add Icosahedron"
-        // name="addIcosahedron"
-        fullWidth
-        variant="filled"
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        data-testid="new-icosahedron-submit-button"
-      >
-        Add
-      </Button>
+      <Box display="flex" className={classes.root}>
+        <TextField
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder="Add Icosahedron"
+          // name="addIcosahedron"
+          fullWidth
+          variant="filled"
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          data-testid="new-icosahedron-submit-button"
+        >
+          Add
+        </Button>
+      </Box>
     </form>
   );
 };
